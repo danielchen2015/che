c3f17788afa4f76ae1f556bb44a892eb0c983f37
michael <471727202@qq.com>
2019-05-08 22:15:43 +0800

    michael vehicle api model

---
+++ application/api/model/Vehicle.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019\5\8 0008
+ * Time: 2:31
+ */
+
+namespace app\api\model;
+use think\Db;
+
+class Vehicle extends Base
+{
+    /**
+     * @param $data
+     * 车辆信息添加
+     */
+    public function vehicleAdd($data)
+    {
+
+        return Db::table('che_vehicle')->data($data)->insert();
+
+    }
+}
